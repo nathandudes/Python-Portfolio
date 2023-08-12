6ee6152b1863e0c4c4ebfec02a59ed5c66dd48f0
nathandudes <nathandelongyt@gmail.com>
2023-08-12 11:52:44 -0700

    Higher or Lower Game

---
+++ HigherLower/HigherLower.py
@@ -0,0 +1,56 @@
+import random
+import HigherLower_art
+import HigherLower_data
+
+print(HigherLower_art.logo)
+NAME = ''
+DESCRIPTION = ''
+COUNTRY = ''
+FOLLOWERS = 0
+FOLLOWERS_A = 0
+FOLLOWERS_B = 0
+SCORE = 0
+GAME_OVER = False
+
+
+def choosing_data():
+    global NAME, DESCRIPTION, COUNTRY, FOLLOWERS
+    for _ in HigherLower_data.data:
+        random_data = random.choice(HigherLower_data.data)
+        NAME = random_data['name']
+        DESCRIPTION = random_data['description']
+        COUNTRY = random_data['country']
+        FOLLOWERS = random_data['follower_count']
+    print(FOLLOWERS)
+
+
+def compare_a():
+    global FOLLOWERS_A
+    choosing_data()
+    FOLLOWERS_A = FOLLOWERS
+    print(f"Compare A: {NAME}, a {DESCRIPTION}, from {COUNTRY}.")
+
+
+def compare_b():
+    global FOLLOWERS_B
+    choosing_data()
+    FOLLOWERS_B = FOLLOWERS
+    print(f"Compare B: {NAME}, a {DESCRIPTION}, from {COUNTRY}.")
+
+
+while not GAME_OVER:
+    compare_a()
+    print(HigherLower_art.vs)
+    compare_b()
+    answer = input("\nWho has more followers? Typer 'A' or 'B'")
+
+    if answer == "A" and FOLLOWERS_A > FOLLOWERS_B:
+        SCORE += 1
+        print(f"You're right! Current score: {SCORE}")
+    elif answer == "B" and FOLLOWERS_B > FOLLOWERS_A:
+        SCORE += 1
+        print(f"You're right! Current score: {SCORE}")
+    else:
+        GAME_OVER = True
+        print(f"Sorry, that's wrong. Final Score: {SCORE}")
+
