bf5042d4370c2ce96c622386b0ea1f59862291b9
nathandudes <nathandelongyt@gmail.com>
2023-08-21 00:26:09 -0700

    Snake Game

--- SnakeGame/Game.py
+++ SnakeGame/Game.py
@@ -34,13 +34,16 @@ while game_is_on:
 
 # Detects collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
+
 
 # Detects collision with tail.
 for segment in snake.segments[1:]:
     if snake.head.distance(segment) < 10:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
+
+
 
 screen.exitonclick()
--- SnakeGame/Scoreboard.py
+++ SnakeGame/Scoreboard.py
@@ -9,20 +9,28 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("high_score.txt") as hs:
+            self.high_score = int(hs.read())
         self.color("White")
         self.penup()
         self.goto(0, 260)
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
         self.hideturtle()
 
-    def game_over(self):
-        self.home()
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
-
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+        with open("high_score.txt", mode="w") as hs:
+            hs.write(f"{self.high_score}")
 
     def increase(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
+
+
